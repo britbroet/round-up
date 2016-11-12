8fb00bf9fab24fcb97f5b07caefaf999df7960fb
britbroet <brittanybroetje@gmail.com>
2016-11-12 10:03:23 -0800

    even more cleanup!

--- models/round.js
+++ models/round.js
@@ -4,10 +4,8 @@ var roundSchema = new mongoose.Schema({
   candidate: { firstName: String, lastName: String},
   position: { title: String, description: String},
   date: String,
-  //questions: [],
   questions: [
-  	{question: String //,
-  		// responses: [{ userId: String, interviewId: String, score: String, notes: String }]
+  	{question: String
   	}],
   interviews: [
   	{name: String}],
@@ -18,16 +16,3 @@ var Round = mongoose.model('Round', roundSchema);
 
 module.exports = Round;
 
-
-
-// var mongoose = require('mongoose');
-
-// var roundSchema = new mongoose.Schema({
-// // some stuff id commented out
-//   candidateId: Number,
-//   positionId: Number
-// });
-
-// var Round = mongoose.model('Round', roundSchema);
-
-// module.exports = Round;
