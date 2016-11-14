384062ad68c13898cf6fe2f6de9543a2f58a4506
britbroet <brittanybroetje@gmail.com>
2016-11-14 07:07:08 -0800

    fixed other bug, still one left

--- public/js/app/components/navBar/navbar.js
+++ public/js/app/components/navBar/navbar.js
@@ -7,25 +7,35 @@
   });
 
 
-  function NavBar($http, $state, $location, Auth, Alerts) {
+  function NavBar($http, $rootScope, $state, $location, Auth, Alerts) {
     var navBar = this;
     navBar.Auth = Auth;
+    navBar.userData = {};
+    navBar.loggedIn = false;
 
     // ACCESS USER DATA
-    var payload = Auth.currentUser();
+    //var payload = Auth.currentUser();
+    navBar.payload = Auth.currentUser();
+    console.log('nav payload', navBar.payload);
 
-    if (payload) {
-      navBar.userData = payload._doc;
+
+    if (navBar.payload) {
+      navBar.loggedIn = true;
+      navBar.userData = navBar.payload._doc;
+      console.log('payload doc', navBar.loggedIn);
+      console.log('user data', navBar.userData);
     }
 
     navBar.logout = function() {
+      console.log("logged out");
     Auth.removeToken();
     console.log('My token:', Auth.getToken());
     Alerts.add('success', 'Logged out!');
+    navBar.loggedIn = false;
     $state.reload();
   	}
   }
 
-  NavBar.$inject = ['$http','$state', '$location', 'Auth','Alerts'];
+  NavBar.$inject = ['$http', '$rootScope', '$state', '$location', 'Auth','Alerts'];
 })()
 
