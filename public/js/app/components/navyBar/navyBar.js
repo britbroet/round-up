5727541879c54e7505ad86d881962f72af7d18a3
britbroet <brittanybroetje@gmail.com>
2016-12-06 18:52:13 -0800

    trying to add navbarthings back in

--- public/js/app/components/navyBar/navyBar.js
+++ public/js/app/components/navyBar/navyBar.js
@@ -8,32 +8,32 @@
 
   function NavyBar($state, Auth) {
     console.log('in navbar controller');
-    // var navBar = this;
-    // navBar.Auth = Auth;
-    // navBar.userData = {};
-    // navBar.loggedIn = false;
+    var navBar = this;
+    navBar.Auth = Auth;
+    navBar.userData = {};
+    navBar.loggedIn = false;
 
-    // // ACCESS USER DATA
-    // //var payload = Auth.currentUser();
-    // navBar.payload = Auth.currentUser();
-    // console.log('nav payload', navBar.payload);
+    // ACCESS USER DATA
+    //var payload = Auth.currentUser();
+    navBar.payload = Auth.currentUser();
+    console.log('nav payload', navBar.payload);
 
 
-    // if (navBar.payload) {
-    //   navBar.loggedIn = true;
-    //   navBar.userData = navBar.payload._doc;
-    //   console.log('payload doc', navBar.loggedIn);
-    //   console.log('user data', navBar.userData);
-    // }
+    if (navBar.payload) {
+      navBar.loggedIn = true;
+      navBar.userData = navBar.payload._doc;
+      console.log('payload doc', navBar.loggedIn);
+      console.log('user data', navBar.userData);
+    }
 
-    // navBar.logout = function() {
-    //   console.log("logged out");
-    // Auth.removeToken();
-    // console.log('My token:', Auth.getToken());
-    // Alerts.add('success', 'Logged out!');
-    // navBar.loggedIn = false;
-    // $state.reload();
-    // }
+    navBar.logout = function() {
+      console.log("logged out");
+    Auth.removeToken();
+    console.log('My token:', Auth.getToken());
+    Alerts.add('success', 'Logged out!');
+    navBar.loggedIn = false;
+    $state.reload();
+    }
   }
 
   NavyBar.$inject = ['$state', 'Auth'];
