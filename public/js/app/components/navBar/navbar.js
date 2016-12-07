27a5ee5f9cd70fac045137456dc319431e0b406e
britbroet <brittanybroetje@gmail.com>
2016-12-06 17:21:24 -0800

    changing services for navbar, comment-saved old version

--- public/js/app/components/navBar/navbar.js
+++ public/js/app/components/navBar/navbar.js
@@ -1,13 +1,13 @@
 (function() {
   angular.module('Roundup')
   .component('navBar', {
-    templateUrl: 'js/app/components/navbar/navbar.html',
+    templateUrl: 'js/app/components/navBar/navBar.html',
     controller: NavBar,
     controllerAs: 'navBar'
   });
 
 
-  function NavBar($http, $rootScope, $state, $location, Auth, Alerts) {
+  function NavBar($state, $location, Auth) {
     var navBar = this;
     navBar.Auth = Auth;
     navBar.userData = {};
@@ -33,9 +33,56 @@
     Alerts.add('success', 'Logged out!');
     navBar.loggedIn = false;
     $state.reload();
-  	}
+    }
   }
 
-  NavBar.$inject = ['$http', '$rootScope', '$state', '$location', 'Auth','Alerts'];
+  NavBar.$inject = ['$state', '$location', 'Auth'];
 })()
+
+
+
+
+
+// old version:
+
+// (function() {
+//   angular.module('Roundup')
+//   .component('navBar', {
+//     templateUrl: 'js/app/components/navBar/navBar.html',
+//     controller: NavBar,
+//     controllerAs: 'navBar'
+//   });
+
+
+//   function NavBar($http, $rootScope, $state, $location, Auth, Alerts) {
+//     var navBar = this;
+//     navBar.Auth = Auth;
+//     navBar.userData = {};
+//     navBar.loggedIn = false;
+
+//     // ACCESS USER DATA
+//     //var payload = Auth.currentUser();
+//     navBar.payload = Auth.currentUser();
+//     console.log('nav payload', navBar.payload);
+
+
+//     if (navBar.payload) {
+//       navBar.loggedIn = true;
+//       navBar.userData = navBar.payload._doc;
+//       console.log('payload doc', navBar.loggedIn);
+//       console.log('user data', navBar.userData);
+//     }
+
+//     navBar.logout = function() {
+//       console.log("logged out");
+//     Auth.removeToken();
+//     console.log('My token:', Auth.getToken());
+//     Alerts.add('success', 'Logged out!');
+//     navBar.loggedIn = false;
+//     $state.reload();
+//   	}
+//   }
+
+//   NavBar.$inject = ['$http', '$rootScope', '$state', '$location', 'Auth','Alerts'];
+// })()
 
